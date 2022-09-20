9d62e2903fa094accb8e486479c7da70b0058f6e
VerdiEza <114009794+VerdiEza@users.noreply.github.com>
2022-09-20 23:00:53 +0700

    Add files via upload

---
+++ PengenalanPbo.java
@@ -0,0 +1,14 @@
+/*
+Nama    : Verdi EZa Irawan
+NIM     : 20210040106
+Kelas   : TI21E
+Tugas Sesi 1
+ */
+public class PengenalanPbo {
+    public static void main(String[] args) {
+        System.out.println("Belajar JAVA");
+        System.out.println("Sangat mudah sekali");
+        System.out.println("dan sangat menyenangkan");
+    }
+    
+}
---
+++ PengenalanPbo2.java
@@ -0,0 +1,19 @@
+/*
+
+Nama    : Verdi Eza Irawan
+NIM     : 20210040106
+Kelas   : TI21E
+ */
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+
+public class PengenalanPbo2 {
+    public static void main(String[] args) {
+        String input;
+        input = JOptionPane.showInputDialog("Anda Sedang belajar apa?");
+        JOptionPane.showMessageDialog(null, "Belajar "+input+" sangat mudah","Message",JOptionPane.INFORMATION_MESSAGE);
+        
+        
+    }
+    
+}
